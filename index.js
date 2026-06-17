7a98d44ffdbea1ceffd584746eb4b95093679d06
King-Corbo <looshan@hotmail.com>
2026-06-17 16:43:48 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,120 @@
+require("dotenv").config();
+const express = require("express");
+const Note = require("./models/note");
+// const cors = require("cors");
+
+const app = express();
+
+app.use(express.static(`dist`));
+app.use(express.json());
+// app.use(cors());
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+//Get all notes
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
+//Fetching a single resource
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+//Delete a specific (id) note
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.status(204).send({ deleted: note });
+      } else {
+        return response.status(400).json({ error: "content missing" });
+      }
+    })
+    .catch((error) => next(error));
+});
+
+//Generate an id for a new note
+// const generateID = () => {
+//   const maxId =
+//     notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+//   return String(maxId + 1);
+// };
+
+//Add a note
+app.post("/api/notes", (request, response, next) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
+});
+
+//Update a specific (id) note
+app.put("/api/notes/:id", (request, response) => {
+  const { content, important } = request.body;
+
+  Note.findById(request.params.id).then((note) => {
+    if (!note) {
+      return response.status(404).end();
+    }
+
+    note.content = content;
+    note.important = important;
+
+    return note.save().then((updatedNote) => {
+      response.json(updatedNote);
+    });
+  });
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+//Error handler as last middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
